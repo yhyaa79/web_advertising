588c5370f142d2f0b279a9d2e95a69949fad2539
yhyaa79 <162740371+yhyaa79@users.noreply.github.com>
2026-09-08 16:18:27 +0330

    Redesign

--- WebAdvertising/settings.py
+++ WebAdvertising/settings.py
@@ -97,6 +97,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
